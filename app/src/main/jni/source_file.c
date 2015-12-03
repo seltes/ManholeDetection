97eb17ec5cf59dc74852628fbe251e23ed120c5a
T.Yasunaga <urygsd.love@gmail.com>
2015-12-03 19:06:52 +0900

    研究

---
+++ app/src/main/jni/source_file.c
@@ -0,0 +1,23 @@
+//
+// Created by i10yasunaga on 2015/11/30.
+//
+
+
+#include "jni.h"
+
+
+void Java_i10_manholedetection_MainActivity_mainTest(JNIEnv * env, jobject  obj,  jintArray src, jint width, jint height)
+{
+    int i, totalPixel;
+    jint* arr  =(*env)->GetIntArrayElements(env,src,0);
+    totalPixel = width * height;
+    for(i=0; i< totalPixel; i++) {
+        int alpha = (arr[i] & 0xFF000000) >> 24;
+        int red   = (arr[i] & 0x00FF0000) >> 16;
+        int green = (arr[i] & 0x0000FF00) >> 8;
+        int blue  = (arr[i] & 0x000000FF);
+        //ここに計算処理を色々と書く。
+        arr[i] = (alpha << 24) | (red << 16) | (green << 8)| blue;
+    }
+    (*env)->ReleaseIntArrayElements(env, src, arr, 0);
+}
