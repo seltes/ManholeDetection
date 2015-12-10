5fe34897c16667c54949299571632252b914da23
T.Yasunaga <urygsd.love@gmail.com>
2015-12-10 11:02:23 +0900

    CameraPreviewActivityがうまく起動していない

---
+++ app/src/androidTest/java/i10/manholedetection/CameraPreview.java
@@ -0,0 +1,106 @@
+package i10.manholedetection;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by i10yasunaga on 2015/12/09.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    SurfaceHolder mHolder;
+    Camera mCamera;
+
+    CameraPreview(Context context) {
+        super(context);
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+        mCamera = Camera.open();
+        try {
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+            mCamera.release();
+            mCamera = null;
+            // TODO: add more exception handling logic here
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(w, h);
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+    }
+
+    public void setCamera(Camera camera) {
+        this.mCamera = camera;
+    }
+
+
+    private boolean mProgressFlag = false;
+
+    private final Camera.PreviewCallback editPreviewImage =
+            new Camera.PreviewCallback() {
+
+                public void onPreviewFrame(byte[] data, Camera camera) {
+                    mCamera.setPreviewCallback(null);  // プレビューコールバックを解除
+
+                    mCamera.stopPreview();
+                    // 画像の保存処理
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MMddHH_mmss_SSS");
+
+                    // 　画像を保存
+                    String path = Environment.getExternalStorageDirectory().getPath() +
+                            '/' + dateFormat.format(new Date()) + ".raw";
+
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(path);
+                        fos.write(data);
+                        fos.close();
+                    } catch (IOException e) {
+                        Log.e("CAMERA", e.getMessage());
+                    }
+
+                    mCamera.startPreview();
+
+                    mProgressFlag = false;
+                }
+            };
+    public void takePreviewRawData() {
+        if (!mProgressFlag) {
+            mProgressFlag = true;
+            mCamera.setPreviewCallback(editPreviewImage);
+            //プレビューコールバックをセット
+        }
+    }
+}
---
+++ app/src/androidTest/java/i10/manholedetection/CameraPreviewActivity.java
@@ -0,0 +1,74 @@
+package i10.manholedetection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.IOException;
+
+
+/**
+ * Created by i10yasunaga on 2015/12/08.
+ */
+public class CameraPreviewActivity extends Activity{
+
+    CameraPreview mPreview;
+    int numberOfCameras;
+    int defaultCameraId;
+    int cameraCurrentlyLocked;
+    Camera mCamera;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //タイトルを非表示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // プレビュー画面を作成
+        mPreview = new CameraPreview(this);
+        setContentView(mPreview);
+
+        // カメラ数を取得
+        numberOfCameras = Camera.getNumberOfCameras();
+
+        // 複数カメラがあった場合に備えて、BACKカメラを指定
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // カメラを開く
+        mCamera = Camera.open();
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // カメラを停止する
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+}
+
+
+
